cf797a45efd9df9b776ddb7035e263095f7b534a
Deema Essam <106381596+DeemaEssam@users.noreply.github.com>
2026-01-07 01:04:06 +0300

    Create project-details.js

---
+++ js/project-details.js
@@ -0,0 +1,15 @@
+const params = new URLSearchParams(window.location.search);
+const projectId = params.get("id");
+
+const project = projects.find(p => p.id === projectId);
+
+if (!project) {
+    document.body.innerHTML = "<h2>Project not found</h2>";
+} else {
+    document.getElementById("title").textContent = project.title;
+    document.getElementById("image").src = project.image;
+    document.getElementById("description").textContent =
+        project.fullDescription;
+    document.getElementById("date").textContent = project.date;
+    document.getElementById("github").href = project.github;
+}
