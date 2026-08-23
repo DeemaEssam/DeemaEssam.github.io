481b5b0ce201a5ac780a5a820f060b8a3fb94f91
Deema Essam <106381596+DeemaEssam@users.noreply.github.com>
2026-08-23 10:04:44 +0300

    Update print statement from 'Hello' to 'Goodbye'

--- Deema/js/projects-data-ar.js
+++ Deema/js/projects-data-ar.js
@@ -343,7 +343,7 @@ const projects = [
         fullDescription:
             `لعبة تعليمية تم تطويرها لمساعدة المتعلمين على ممارسة الأرقام كجزء من دورة تعلم اللغة الألمانية.
     
-    ★ <a href="https://gger.netlify.app/index.html" target="_blank" style="color:#4CAF50;font-weight:bold;">جرب اللعبة</a>
+    🌐 <a href="https://gger.netlify.app/index.html" target="_blank" style="color:#4CAF50;font-weight:bold;">جرب اللعبة</a>
     `,
         date: "Apr 19, 2023",
         github: "https://github.com/DeemaEssam/German-Numbers-Learning-Game"
