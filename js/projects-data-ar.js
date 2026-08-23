71b7d9a12e524eef773cd8134502beb358707cbf
Deema Essam <106381596+DeemaEssam@users.noreply.github.com>
2026-08-23 08:05:08 +0300

    Update projects-data-ar.js

--- js/projects-data-ar.js
+++ js/projects-data-ar.js
@@ -352,8 +352,8 @@ const projects = [
         shortDescription:
             "لعبة تعليمية لتعلم الأرقام باللغة الألمانية.",
         fullDescription:
-            "لعبة تعليمية تم تطويرها لمساعدة المتعلمين على ممارسة الأرقام كجزء من دورة تعلم اللغة الألمانية.",
-            🔹 <a href="https://deemaessam.github.io/projects/Ataa/index.html" target="_blank" style="color:#4CAF50;font-weight:bold;">معاينة لوحة المعلومات</a>
+            "لعبة تعليمية تم تطويرها لمساعدة المتعلمين على ممارسة الأرقام كجزء من دورة تعلم اللغة الألمانية.
+            🔹 <a href="https://gger.netlify.app/index.html" target="_blank" style="color:#4CAF50;font-weight:bold;">تجربة اللعبة</a>",
         date: "Apr 19, 2023",
         github: ""
     },
