9c3c011d694d7a1f2cdf752b5183dfd9eea9fea7
Deema Essam <106381596+DeemaEssam@users.noreply.github.com>
2026-09-21 02:57:11 +0300

    Update projects-data-ar.js

--- js/projects-data-ar.js
+++ js/projects-data-ar.js
@@ -339,7 +339,7 @@ const projects = [
         title: "موقع قائمة الطعام",
         category: "web",
         tools: ["HTML", "CSS", "JavaScript", "PHP"],
-        image: "https://deemaessam.github.io/pics/pizza.gif",
+        image: "https://deemaessam.github.io/pics/pizza.png",
         shortDescription:
             "موقع قائمة طعام لمطعم مع ميزات التقييم.",
         fullDescription:
